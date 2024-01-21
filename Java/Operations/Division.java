8c6dbabb1fe9c6ca73a692686297bb6a078ab0af
ItzRynn <152287136+ItzRynn@users.noreply.github.com>
2024-01-21 19:33:08 +0100

    Update Division.java

--- Java/Operations/Division.java
+++ Java/Operations/Division.java
@@ -5,9 +5,15 @@ public class Division extends Calculator {
     @Override
     public double calculate(double num1, double num2) {
 
-        assert num2 != 0 : "Divisor must not be 0."; // Checks if the divisor is 0
+        assert num2 != 0 : "Divisor must not be 0."; // Example of assertion
 
-        return num1 / num2;
+        // Checks if the divisor is 0
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Divisor must not be 0.");
+        }
+        else {
+            return num1 / num2;
+        }
     }
 
     public void calculate(long num) {
