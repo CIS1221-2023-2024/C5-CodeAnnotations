7bf5dc2b5a9d7953e81cc424b5b1a8b8c3e0c106
ItzRynn <152287136+ItzRynn@users.noreply.github.com>
2024-01-21 10:24:46 +0100

    Update and rename Multiplication to Multiplication.java

---
+++ Java/Operations/Multiplication.java
@@ -0,0 +1,13 @@
+package Operations;
+import Calculator.*;
+// Multiplication operation
+public class Multiplication extends Calculator {
+    @Override
+    public double calculate(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public void calculate(long num) {
+
+    }
+}
