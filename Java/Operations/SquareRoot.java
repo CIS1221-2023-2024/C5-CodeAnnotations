50ecd8e56f9a6630a107da23a58b17f1b721caac
ItzRynn <152287136+ItzRynn@users.noreply.github.com>
2024-01-21 19:33:34 +0100

    Update SquareRoot.java

--- Java/Operations/SquareRoot.java
+++ Java/Operations/SquareRoot.java
@@ -5,9 +5,14 @@ public class SquareRoot extends Calculator {
     @Override
     public void calculate(long num) {
 
-        assert num >= 0 : "Number needs to be greater or equal to 0."; // Checks if number is greater or equal to 0
+        assert num >= 0 : "Cannot calculate the square root of a negative number."; // Example of assertion
 
-        System.out.println(Math.sqrt(num));
+        // Checks if number is greater or equal to 0
+        if (num < 0) {
+            System.out.println("Cannot calculate the square root of a negative number.");
+        } else {
+            System.out.println(Math.sqrt(num));
+        }
     }
 
     public double calculate(double num1, double num2) {
