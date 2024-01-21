6c05591ca0c333012fd86c1f591808a8982c0271
ItzRynn <152287136+ItzRynn@users.noreply.github.com>
2024-01-21 10:24:00 +0100

    Update and rename Addition to Addition.java

---
+++ Java/Operations/Addition.java
@@ -0,0 +1,13 @@
+package Operations;
+import Calculator.*;
+// Addition operation
+public class Addition extends Calculator {
+    @Override
+    public double calculate(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public void calculate(long num) {
+
+    }
+}
